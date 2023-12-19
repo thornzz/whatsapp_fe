727e524dab7ac2e2cae4810b3bd4e4d6c7379fa6
Ibrahim Akgun <ibrahim@k2mbilisim.com>
2023-12-20 01:01:50 +0300

    Typing fixed

--- src/components/Chat/actions/Input.jsx
+++ src/components/Chat/actions/Input.jsx
@@ -1,28 +1,42 @@
-import { useState } from "react";
+import { useState, useEffect,useRef } from "react";
 import { useSelector } from "react-redux";
 import SocketContext from "../../../context/SocketContext";
 
 function Input({ message, setMessage, textRef, socket }) {
   const { activeConversation } = useSelector((state) => state.chat);
-
   const [typing, setTyping] = useState(false);
+  const typingTimer = useRef(null); // useRef hook'unu kullanarak typingTimer değişkenini tanımlayın
   const onChangeHandler = (e) => {
     setMessage(e.target.value);
     if (!typing) {
       setTyping(true);
+      console.log('typing true oldu');
       socket.emit("typing", activeConversation._id);
     }
-    let lastTypingTime = new Date().getTime();
-    let timer = 1000;
-    setTimeout(() => {
-      let timeNow = new Date().getTime();
-      let timeDiff = timeNow - lastTypingTime;
-      if (timeDiff >= timer && typing) {
-        socket.emit("stop typing", activeConversation._id);
-        setTyping(false);
-      }
-    }, timer);
+
+    // Eğer bir zamanlayıcı zaten çalışıyorsa, yeni bir tane başlatma
+    if (typingTimer.current) {
+      clearTimeout(typingTimer.current);
+    }
+
+    // Zamanlayıcıyı başlat
+    typingTimer.current = setTimeout(stopTyping, 2000);
   };
+
+  const stopTyping = () => {
+    console.log('stop typing tetiklendi');
+    socket.emit("stop typing", activeConversation._id);
+    setTyping(false);
+    typingTimer.current = null; // Zamanlayıcıyı sıfırla
+  };
+
+  useEffect(() => {
+    return () => {
+      // Komponent unmount olduğunda zamanlayıcıyı temizle
+      clearTimeout(typingTimer.current);
+    };
+  }, []);
+
   return (
     <div className="w-full">
       <input
