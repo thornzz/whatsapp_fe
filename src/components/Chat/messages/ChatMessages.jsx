c563ed45d349070abfe9c813de5742c14268b18e
Ibrahim Akgun <ibrahim@k2mbilisim.com>
2023-12-21 10:48:57 +0300

    fixed some warning messages

--- src/components/Chat/messages/ChatMessages.jsx
+++ src/components/Chat/messages/ChatMessages.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import React , { useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
 import Message from "./Message";
 import Typing from "./Typing";
@@ -8,31 +8,33 @@ export default function ChatMessages({ typing }) {
   const { messages, activeConversation } = useSelector((state) => state.chat);
   const { user } = useSelector((state) => state.user);
   const endRef = useRef();
+
   useEffect(() => {
     scrollToBottom();
   }, [messages, typing]);
+
   const scrollToBottom = () => {
     endRef.current.scrollIntoView({ behavior: "smooth" });
   };
+
   return (
     <div
       className="mb-[60px] bg-[url('https://res.cloudinary.com/dmhcnhtng/image/upload/v1677358270/Untitled-1_copy_rpx8yb.jpg')]
-    bg-cover bg-no-repeat
-    "
+      bg-cover bg-no-repeat"
     >
       {/*Container*/}
       <div className="scrollbar overflow_scrollbar overflow-auto py-2 px-[5%]">
         {/*Messages*/}
         {messages &&
           messages.map((message) => (
-            <>
+            <React.Fragment key={message._id}>
               {/*Message files */}
               {message.files.length > 0
-                ? message.files.map((file) => (
+                ? message.files.map((file, index) => (
                     <FileMessage
                       FileMessage={file}
                       message={message}
-                      key={message._id}
+                      key={`${message._id}_file_${index}`}
                       me={user._id === message.sender._id}
                     />
                   ))
@@ -41,13 +43,13 @@ export default function ChatMessages({ typing }) {
               {message.message.length > 0 ? (
                 <Message
                   message={message}
-                  key={message._id}
+                  key={`${message._id}_text`}
                   me={user._id === message.sender._id}
                 />
               ) : null}
-            </>
+            </React.Fragment>
           ))}
-        {typing === activeConversation._id ? <Typing /> : null}
+        {typing === activeConversation._id ? <Typing key="typing" /> : null}
         <div className="mt-2" ref={endRef}></div>
       </div>
     </div>
--- src/components/Chat/messages/files/FileMessage.jsx
+++ src/components/Chat/messages/files/FileMessage.jsx
@@ -35,7 +35,7 @@ export default function FileMessage({ FileMessage, message, me }) {
         `}
         >
           {/*Message*/}
-          <p
+          <div
             className={`h-full text-sm ${
               type !== "IMAGE" && type !== "VIDEO" ? "pb-5" : ""
             }`}
@@ -45,7 +45,7 @@ export default function FileMessage({ FileMessage, message, me }) {
             ) : (
               <FileOthers file={file} type={type} me={me} />
             )}
-          </p>
+          </div>
           {/*Message Date*/}
           <span className="absolute right-1.5 bottom-1.5 text-xs text-dark_text_5 leading-none">
             {moment(message.createdAt).format("HH:mm")}
